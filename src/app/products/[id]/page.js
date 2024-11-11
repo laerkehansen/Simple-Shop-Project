bf6148990482a2dd97e994f7df0b9afb6fc12304
laerkehansen <laha0007@stud.kea.dk>
2024-11-11 15:05:13 +0100

    productlist og singleview

---
+++ src/app/products/[id]/page.js
@@ -0,0 +1,33 @@
+import Image from "next/image";
+
+const Product = async ({ params }) => {
+  const id = (await params).id;
+  let response = await fetch(`https://dummyjson.com/products/${id}`);
+  let product = await response.json();
+  return (
+    <div>
+      <a href="/products">Tilbage</a>
+      <Image
+        src={`${product.thumbnail}`}
+        height={250}
+        width={250}
+        alt={product.title}
+      ></Image>
+      <ul className="flex gap-2">
+        {product.images.map((image) => (
+          <Image
+            src={`${image}`}
+            height={100}
+            width={100}
+            alt={product.title}
+          ></Image>
+        ))}
+      </ul>
+      {/* <div>{product.images}</div> */}
+      <h1>{product.title}</h1>
+      <p>{product.description}</p>
+    </div>
+  );
+};
+
+export default Product;
--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,8 +1,28 @@
-const Products = () => {
+import Image from "next/image";
+import Link from "next/link";
+
+const Products = async ({}) => {
+  let response = await fetch("https://dummyjson.com/products");
+  let data = await response.json();
   return (
-    <div>
-      <h1>Products</h1>
-    </div>
+    <ul>
+      {data.products.map((product) => (
+        <li key={product.id}>
+          <Link href={`/products/${product.id}`}>
+            <Image
+              src={`${product.thumbnail}`}
+              alt={product.title}
+              width={250}
+              height={250}
+            ></Image>
+          </Link>
+          <h1>{product.title}</h1>
+          <h2>{product.brand}</h2>
+          <p>{product.price}</p>
+          <button>Tilføj til kurv</button>
+        </li>
+      ))}
+    </ul>
   );
 };
 
