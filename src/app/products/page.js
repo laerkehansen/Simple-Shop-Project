6800fe2a948c6477647d0b9c31be2da34c420955
laerkehansen <laha0007@stud.kea.dk>
2024-11-12 11:55:21 +0100

    card ish

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,14 +1,21 @@
 import Image from "next/image";
 import Link from "next/link";
+import Star from "../img/star.svg";
 
 const Products = async ({}) => {
   let response = await fetch("https://dummyjson.com/products");
   let data = await response.json();
   return (
-    <ul>
+    <section className="grid grid-cols-[repeat(2,minmax(0,450px))] justify-center gap-8">
       {data.products.map((product) => (
-        <li key={product.id}>
-          <Link href={`/products/${product.id}`}>
+        <div
+          key={product.id}
+          className="bg-orange-200 text-orange-950 rounded-3xl grid pt-6"
+        >
+          <Link
+            href={`/products/${product.id}`}
+            className="justify-self-center"
+          >
             <Image
               src={`${product.thumbnail}`}
               alt={product.title}
@@ -16,13 +23,24 @@ const Products = async ({}) => {
               height={250}
             ></Image>
           </Link>
-          <h1>{product.title}</h1>
-          <h2>{product.brand}</h2>
-          <p>{product.price}</p>
-          <button>Tilføj til kurv</button>
-        </li>
+          <div className="grid grid-rows-[0.2fr_1fr_0.5fr] p-6">
+            <Image src={Star} />
+            <div className="flex justify-between">
+              <div className="">
+                <h1 className="text-xl font-bold w-64">{product.title}</h1>
+                <h2 className="font-thin">{product.brand}</h2>
+              </div>
+              <p>{product.price}$</p>
+            </div>
+            <div className="flex justify-end">
+              <button className="bg-red-400 rounded-xl px-3 py-2 w-fit text-white">
+                Tilføj til kurv
+              </button>
+            </div>
+          </div>
+        </div>
       ))}
-    </ul>
+    </section>
   );
 };
 
