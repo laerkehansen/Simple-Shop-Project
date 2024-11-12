3fe19ff32c1fb80e0475f0dade0afd28cb19a92c
laerkehansen <laha0007@stud.kea.dk>
2024-11-12 10:55:40 +0100

    header 1st try

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,9 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import Link from "next/link";
+import Image from "next/image";
+import Logo from "../app/img/logo.svg";
+import Basket from "../app/img/basket.svg";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -22,17 +25,21 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white`}
       >
-        <header>
-          <nav>
-            <ul>
+        <header className="">
+          <nav className="flex bg-pink-200 text-pink-400 py-6 text-xl justify-between px-10">
+            <Link href="/">
+              <Image src={Logo} alt="logo af brandet" />
+            </Link>
+            <ul className="flex gap-4 items-center">
               <li>
                 <Link href="/">Home</Link>
               </li>
               <li>
                 <Link href="/products">Products</Link>
               </li>
+              <Image src={Basket} alt="illustration af kurv" />
             </ul>
           </nav>
         </header>
