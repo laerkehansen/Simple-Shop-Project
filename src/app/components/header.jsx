ed6665afa4ffd5ba25999242d108366302a3a285
laerkehansen <laha0007@stud.kea.dk>
2024-11-12 09:44:04 +0100

    prøve med components

---
+++ src/app/components/header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Products</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
