bff7ab973d1187cfd252b349971deda8effb2c65
”sagecave” <Andreassage@gmail.com>
2024-11-12 14:50:25 +0100

    single grid virker, og lavet nogle små juteringer

--- src/app/components/Gallery.jsx
+++ src/app/components/Gallery.jsx
@@ -5,13 +5,13 @@ import { useState } from "react";
 const Gallery = ({ thumbnail, images, title }) => {
   const [selectedProduct, setSelectedProduct] = useState(thumbnail);
   return (
-    <div className="grid gap-4">
-      <div className="bg-[--dark-grey] grid place-content-center rounded-xl">
-        <Image src={selectedProduct} height={250} width={250} alt={title}></Image>
+    <div className="grid gap-2">
+      <div className="bg-primary-gray grid place-content-center rounded-xl ">
+        <Image src={selectedProduct} height={500} width={500} alt={title}></Image>
       </div>
       <ul className="flex gap-2">
         {images.map((image, id) => (
-          <li key={id} className="bg-[--dark-grey] rounded-sm">
+          <li key={id} className="bg-primary-gray rounded-md h-fit w-fit ">
             <button onClick={() => setSelectedProduct(image)}>
               <Image src={image} height={100} width={100} alt={title} />
             </button>
--- src/app/products/[id]/page.js
+++ src/app/products/[id]/page.js
@@ -8,23 +8,30 @@ const Product = async ({ params }) => {
   let product = await response.json();
 
   return (
-    <section className="">
+    <section className="col-start-2 col-end-6 pt-4 pb-28">
       <a href="/products">Tilbage</a>
       <div className="grid-cols-[2fr_1fr] grid gap-5">
         <Gallery thumbnail={product.thumbnail} images={product.images} title={product.title} />
-        <div>
-          <ul className="flex gap-2 text-gray-700 text-[16px]">
-            {product.tags.map((tag) => (
-              <li className="" key={tag}>
-                {tag}
-              </li>
-            ))}
-          </ul>
-          <h1 className="text-3xl">{product.title}</h1>
-          <p className="text-[17px] text-gray-700">{product.brand} </p>
-          <p>{product.price} kr</p>
+        <div className="grid">
+          <div>
+            <ul className="flex gap-2 text-gray-400 text-[16px] ">
+              {product.tags.map((tag) => (
+                <li className="" key={tag}>
+                  {tag}
+                </li>
+              ))}
+            </ul>
+
+            <h1 className="text-3xl">{product.title}</h1>
+            <p className="text-[17px] text-gray-700">{product.brand} </p>
+            <p>{product.price} kr,-</p>
+          </div>
+
           {/* <p>{product.discountPercentage}</p> */}
-          <Button></Button>
+          <div>
+            <Button></Button>
+          </div>
+
           <p>{product.description}</p>
 
           <Accordion dimensions={product.dimensions} warrantyInformation={product.warrantyInformation} shippingInformation={product.shippingInformation} sku={product.sku} />
