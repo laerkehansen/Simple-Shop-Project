6c2c31eb8d5150e1eaabb439bdb4dc88064b98b3
laerkehansen <laha0007@stud.kea.dk>
2024-11-13 15:01:27 +0100

    basket hul igennem

--- src/app/components/Button.jsx
+++ src/app/components/Button.jsx
@@ -1,5 +1,15 @@
+"use client";
 const Button = () => {
-  return <button className="px-8 py-2 bg-[--purple] text-white rounded-md">Add to cart</button>;
+  return (
+    <button
+      onClick={() => {
+        console.log("isOpen", addItem, newItem, key);
+      }}
+      className="px-8 py-2 bg-[--purple] text-white rounded-md"
+    >
+      Add to cart
+    </button>
+  );
 };
 
 export default Button;
